a910f9b2ff84da2cebf7a6ca7876be348b9eea57
othman <anaothman2006@gmail.com>
2026-02-22 13:21:28 +0000

    Refactor: merge grid creation into a single function

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ const content = document.querySelector(".content");
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
-for (let i = 0; i < 256; i++) {
+const button = document.querySelector(".button");
+
+function createGrid(size) {
+    const sqaureDivs = size * size;
+    for (let i = 0; i < sqaureDivs; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square-div");
     content.appendChild(squareDiv);
-    squareDiv.style.width = "6.25%";
+    squareDiv.style.width = 100 / size+"%";
     let opacity = 0;
     squareDiv.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "#333333";
-    opacity = opacity + 0.1;
+    event.target.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    opacity += 0.1;
     event.target.style.opacity = opacity;
-})}
-
-const squareDiv = document.querySelector(".square-div");
-
-const button = document.querySelector(".button");
-
+    });
+    }
+}
 
 function getUserChoice() {
     content.textContent = "";
@@ -25,17 +26,8 @@ function getUserChoice() {
     while (choice >= 100) {
         choice = +prompt("Please enter a number less than 100:");
     }
-    console.log(choice)
-    const result = choice * choice;
-    for (let i = 0; i < result; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add("square-div");
-    content.appendChild(squareDiv);
-    squareDiv.style.width = 100 / choice+"%";
-    squareDiv.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = `#333333`;
-    });
-    }
+    createGrid(choice);
 }
-
 button.addEventListener("click", getUserChoice);
+const defaultGrid = 16;
+createGrid(defaultGrid);
